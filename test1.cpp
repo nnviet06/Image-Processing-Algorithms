f1ee8d8f8872ee3a89c6b53f7bbca4b10895d867
nnviet06 <nnviet2016@gmail.com>
2025-11-13 15:25:27 -0500

    Update setup

---
+++ test1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string name;
+
+    
+    cout << "Nhap ten cua ban: ";
+
+    
+    getline(cin, name);
+
+    
+    cout << "Xin chao " << name << ", chuc ban mot ngay vui ve!" << endl;
+
+    return 0;
+}
